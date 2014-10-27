5e3d0cbd925923c9afc99f6493a2f4dddaf9245b
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2014-10-27 21:38:29 +0100

    Adds `pantomime` as dependency

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
                  [clj-http "1.0.0"]
                  [clojurewerkz/scrypt "1.2.0"]
                  [com.novemberain/monger "2.0.0"]
+                 [com.novemberain/pantomime "2.3.0"]
                  [compojure "1.2.0"]
                  [crypto-random "1.2.0"]
                  [org.clojure/clojure "1.6.0"]

6521ab40b8d42a56fef455c57902c805ac0deb4b
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2014-11-01 14:51:16 +0100

    Implement the login page

--- src/client/teamwall/login.cljs
+++ src/client/teamwall/login.cljs
@@ -1,6 +1,36 @@
 (ns login
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [repository]))
 
+(defn- login-action
+  ""
+  [e email password on-login]
+  (.preventDefault e)
+  (repository/login email
+                    password
+                    on-login))
+
+(defn- render-form
+  ""
+  [on-login]
+  (let [email    (reagent/atom "")
+        password (reagent/atom "")]
+    [:form {:name       "login-form"}
+     "Email: "
+     [:input {:type      "email"
+              ;;             :value     @email
+              :on-change #(reset! email (-> % .-target .-value))
+              :name      "email"}]
+     "Password: "
+     [:input {:type      "password"
+              :on-change #(reset! password (-> % .-target .-value))
+              :name      "password"}]
+     [:input {:type      "submit"
+              :on-click  (fn [event] (login-action event
+                                                   @email
+                                                   @password
+                                                   on-login))
+              :value    "Log in"}]]))
 
 
 
@@ -13,6 +43,7 @@
 
 (defn render-content
   "Main rendering function."
-  []
+  [on-login]
   [:div.login
-   [:h1 "Login"]])
+   [:h1 "Login"]
+   [render-form on-login]])

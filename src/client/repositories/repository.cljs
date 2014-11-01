3ccc6260a91954877f6d987daa2e6a5d3dafe6ba
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2014-11-01 14:51:03 +0100

    Uses ajax for requests

--- src/client/repositories/repository.cljs
+++ src/client/repositories/repository.cljs
@@ -1,51 +1,30 @@
 (ns repository
-  (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [cemerick.url :refer (url url-encode)]
-            [cljs.core.async :as async :refer [<! >! put! chan close!]]
+  (:require [ajax.core :refer [GET POST]]
+            [cemerick.url :refer (url url-encode)]
             [cognitect.transit :as transit]
-            [goog.net.XhrIo :as xhr]
             [goog.string :as gstring]
-            [goog.string.format :as gformat]
-            [chord.client :as chord]))
+            [goog.string.format :as gformat]))
 
 
 (def ^{:private :true} login-url
   "URL for the login route"
   "/login")
 
+(defn- async-get-json
+  [& {:keys [handler url error params]}]
+  (let [options (atom {:handler handler
+                       :response-format :json
+                       :keywords? true})]
+    (if-not (nil? error)
+      (swap! options assoc :error-handler error))
+    (if-not (nil? params)
+      (swap! options assoc :params params))
+    (GET url @options)))
 
-(defn- GET [url]
-  (let [ch (chan 1)]
-    (xhr/send url
-              (fn [event]
-                (let [res (-> event .-target .getResponseText)]
-                  (go (>! ch res)
-                      (close! ch)))))
-    ch))
-
-(defn- convert-result [data]
-  (let [reader (transit/reader :json)]
-    (js->clj (transit/read reader
-                           data)
-             :keywordize-keys true)))
-
-(defn- async-get-json [fn url]
-  (go
-   (fn (convert-result (<! (GET url))))))
-
-
-(defn- build-login-url
-  "Builds the correct URL based on the
-  provided email and password"
-  [email password]
-  (let [full-url (str login-url "?email=%s&password=%s")]
-    (gstring/format full-url
-            (url-encode email)
-            (url-encode password))))
-
-(defn ^:export login
+(defn login
   "Do a request over the login REST API"
   [email password callback]
-  (async-get-json callback
-                  (build-login-url email
-                                   password)))
+  (async-get-json :handler callback
+                  :url login-url
+                  :params {:email    email
+                           :password password}))

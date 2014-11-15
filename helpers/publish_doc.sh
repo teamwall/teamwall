7f7e087b158db2896b263e7d065dfe6cb7020f5a
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2014-11-15 02:12:39 +0100

    Modify publish script to avoid small issue and remove debugging

--- helpers/publish_doc.sh
+++ helpers/publish_doc.sh
@@ -4,21 +4,13 @@ if [[ $TRAVIS_BRANCH == "master" ]];
 then
 	lein doc
 	git clone https://$GH_NAME:$GH_TOKEN@github.com/teamwall/teamwall.git -b gh-pages ../gh-pages
-	cp -r doc ../gh-pages/doc
+	cp -r doc ../gh-pages/
 	cd ../gh-pages
-	pwd
-	echo "git config user.email $GH_EMAIL"
 	git config user.email "$GH_EMAIL"
-	echo "git config user.name $GH_NAME"
 	git config user.name "$GH_NAME"
-	echo 'git status'
 	git status
-	echo 'git add --all .'
 	git add --all .
-	echo 'git commit -am "Generation of documentation"'
 	git commit -am "Generation of documentation"
-	echo 'git status'
 	git status
-	echo "git push https://$GH_NAME:$GH_TOKEN@github.com/teamwall/teamwall.git gh-pages"
 	git push https://$GH_NAME:$GH_TOKEN@github.com/teamwall/teamwall.git gh-pages
 fi
